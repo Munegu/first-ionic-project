014f91a40afbb8c645565e2f4f1e785e28501e36
Nicolas <nicolas.villetelle@hotmail.fr>
2021-06-16 13:15:50 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { OpenTriviaService } from '../open-trivia.service';
+import { Question } from '../question';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,95 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  difficulty: Array<string> = ['Easy','Medium','Hard'];
+  difficultyChoice: string = '';
+  save: boolean = false;
+  pseudo : string = '';
+  valid: boolean = false;
+  questions: Question[] = [];
+  responses: string[] = [];
+  currentQuestion: Question;
+  indexQuestion: number;
+  answered: boolean = false;
+  end: boolean =  false;
+  score: number = 0;
+  randomInt = Math.floor(Math.random() * 9) + 1;
+  randomUser: string = `https://randomuser.me/api/portraits/lego/${this.randomInt}.jpg`;
+
+
+  constructor(
+    private toastController: ToastController,
+    private openTriviaService: OpenTriviaService
+    ) {}
+
+  async presentToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      color: color    });
+    toast.present();
+  }
+
+
+
+  async startQuiz ()   {
+
+    this.end = false;
+    this.answered = false;
+    this.score = 0;
+    this.indexQuestion = 0;
+
+    if (0 === this.difficultyChoice.trim().length || 3 > this.pseudo.trim().length){
+      this.presentToast('Veuillez renseignez un pseudo (3 caractères min) et choisir une difficulté', 'danger');
+    }else {
+      this.valid = true;
+        await this.openTriviaService.getQuestions(10, this.difficultyChoice).then(res => {
+          this.questions = res;
+          this.setQuestion();
+        })
+    }
+  }
+
+  async setQuestion (): Promise<void> {
+    this.currentQuestion = this.questions[this.indexQuestion];
+    this.responses = await this.openTriviaService.getAnswers(this.currentQuestion);
+  }
+
+  setColor (response: string): string {
+    if (!this.answered) {
+      return "medium";
+    }
+    else if (this.answered && this.currentQuestion.correct_answer === response) {
+      return "success";
+    }
+    else {
+      return "danger";
+    }
+  }
+
+
+  response (response: string): void {
+    if (!this.answered && this.currentQuestion.correct_answer === response) {
+      this.score++;
+    }
+      this.answered = true;
+  }
+
+  nextQuestion (): void {
+    if (this.answered && this.indexQuestion+1 <= this.questions.length && undefined != this.currentQuestion){
+      this.answered = false;
+      this.indexQuestion ++;
+      this.setQuestion();
+      if (this.indexQuestion+1 == this.questions.length) {
+        this.end = true;
+      }
+    } 
+    else if (!this.answered) {
+      this.presentToast('Veuillez choisir une réponse !', 'danger');
+    }
+   
+
+  }
+
 
 }
---
+++ src/app/open-trivia.service.ts
@@ -0,0 +1,57 @@
+import { Question } from './question';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenTriviaService {
+
+  constructor(private http: HttpClient) { }
+
+   questions: Question[] = [];
+   
+
+ private async shuffle(array: any[]): Promise<any[]> {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  async callAPI (numberQuestions: number,difficultySelected: string ): Promise<void> {
+    await this.http.get(`https://opentdb.com/api.php?amount=${numberQuestions}&category=15&difficulty=${difficultySelected}`).toPromise()
+    .then((response: any) => {
+      if (0 === response['response_code']){
+      this.questions = response['results'];
+    }
+   })
+    .catch((err) => { console.log(err); });
+
+  }
+
+
+   async getQuestions(numberQuestions: number,difficultySelected: string ): Promise<Question[]> {
+      await this.callAPI(numberQuestions, difficultySelected.toLowerCase());
+      let shuffleQuestions = await this.shuffle(this.questions);
+      return shuffleQuestions;
+  }
+
+  async getAnswers(question: Question): Promise<string[]> {
+    const responses: string[] = [];
+
+    question.incorrect_answers.forEach(answer => {
+      responses.push(answer);
+      
+    });
+
+    responses.push(question.correct_answer);
+
+    return await this.shuffle(responses);
+  }
+}
